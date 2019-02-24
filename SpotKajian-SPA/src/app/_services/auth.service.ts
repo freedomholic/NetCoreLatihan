e26e9257f818da86962d010933826686d2f5a6b5
arka.yudha <yudha@arkamaya.co.id>
2019-02-24 17:28:15 +0700

    Add Register Action And Integrate with API

--- SpotKajian-SPA/src/app/_services/auth.service.ts
+++ SpotKajian-SPA/src/app/_services/auth.service.ts
@@ -8,15 +8,19 @@ import {map} from 'rxjs/operators';
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
 constructor(private http: HttpClient) { }
-  login(model: any){
+  login(model: any) {
     return this.http.post(this.baseUrl + 'login', model)
       .pipe(
-        map((response: any) =>{
+        map((response: any) => {
           const user = response;
           if (user) {
             localStorage.setItem('token', user.token);
           }
         })
-      )
+      );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
   }
 }
--- SpotKajian.API/Controllers/ValuesController.cs
+++ SpotKajian.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace SpotKajian.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
